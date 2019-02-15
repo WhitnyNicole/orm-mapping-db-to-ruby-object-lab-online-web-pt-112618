125dae546e89aa30a265812bd3c1c409a3f217c4
Whitny Edwards <wne4e2@gmail.com>
2019-02-15 01:24:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,5 @@
 class Student
   attr_accessor :id, :name, :grade
-
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
-  end
-
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-  end
-
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
-  end
   
   def save
     sql = <<-SQL
@@ -40,4 +26,61 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-end
+  
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end 
+    
+    
+    def self.find_by_name (name)
+      sql = <<-SQL
+      SELECT * 
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+    
+    def self.all
+    sql = <<-SQL 
+      SELECT *
+      FROM students
+    SQL
+  DB[:conn].execute(sql).map do |row| 
+  self.new_from_db(row)
+    end 
+  end 
+
+ def self.all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = 9"
+    DB[:conn].execute(sql)
+  end
+
+    def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12;"
+    DB[:conn].execute(sql)
+  end
+
+   def self.first_x_students_in_grade_10(num)
+    sql = "SELECT * FROM students WHERE grade=10 ORDER BY students.id LIMIT ?;"
+    DB[:conn].execute(sql, num)
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    first_student_row = DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student_row)
+  end
+
+   def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, x)
+  end
+end 
